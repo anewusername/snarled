35dbc1a4ff861bd5f0a58566dd1824997b1c4867
jan <jan@orange.mpxd.net>
2022-03-30 23:57:50 -0700

    layer can be any Hashable type

--- snarl/types.py
+++ snarl/types.py
@@ -1,5 +1,5 @@
-from typing import Union, Tuple, List, Sequence, Optional
+from typing import Union, Tuple, List, Sequence, Optional, Hashable
 
-layer_t = Tuple[int, int]
+layer_t = Hashable
 contour_t = List[Tuple[int, int]]
 connectivity_t = Sequence[Tuple[layer_t, Optional[layer_t], layer_t]]
